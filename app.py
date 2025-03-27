27331da48ce7c56880a45e91bd882a4df05fb3f8
GabrielR-Sant05 <gabrielsantos88974@outlook.com>
2025-03-27 14:48:00 -0300

    reupload

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
-    return "Hello World", 200
+    return "Olá, Seja Bem Vindo", 200
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "Página Principal", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
